4125f143578da014d2e24ec7f8e3458d3d0f3c7c
Lavanya R <lavanya.r2024cse@sece.ac.in>
2025-08-26 11:57:23 +0530

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,15 +1,28 @@
+// Course.java
+// Represents a Course in the Course Registration System
+
 public class Course {
     private int courseId;
     private String courseName;
     private int credits;
 
+    // Constructor
     public Course(int courseId, String courseName, int credits) {
         this.courseId = courseId;
         this.courseName = courseName;
         this.credits = credits;
     }
 
-    // Methods
+    // Getters & Setters
+    public int getCourseId() { return courseId; }
+    public void setCourseId(int courseId) { this.courseId = courseId; }
+
+    public String getCourseName() { return courseName; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+
+    public int getCredits() { return credits; }
+    public void setCredits(int credits) { this.credits = credits; }
+
     public void addCourse() {
         System.out.println("Course " + courseName + " added.");
     }
